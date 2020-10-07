bab42447ae6b07b8e7e6f441ad19c9f16a282061
Ini Olorunnishola <olo_ini@Inis-MacBook-Air.local>
2020-10-07 18:56:47 -0500

    First commit

---
+++ src/build/precache-manifest.626092d869fe065e2be8a6b58eb8afeb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12af49a00181268a55847a94f2ee3b93",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b9e950fcfd48df6d6dec",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "f7742bd7ca531cfad47f",
+    "url": "/static/js/2.b09d3ca3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b09d3ca3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9e950fcfd48df6d6dec",
+    "url": "/static/js/main.f3707c3d.chunk.js"
+  },
+  {
+    "revision": "8931283fa7532b66ee7f",
+    "url": "/static/js/runtime-main.fca178c7.js"
+  }
+]);
